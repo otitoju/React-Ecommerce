d549582b83b7ea49ac68362275a0aacc7f63863c
otitoju john oluwapelumi <otitojuoluwapelumi@gmail.com>
2022-07-05 00:19:21 -0700

    setup  client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
 
-function App() {
+const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/products/:category" element={<ProductList />}/>
+        <Route path="/product/:id" element={<Product />}/>
+        <Route path="/cart" element={<Cart />}/>
+        <Route path="/success" element={<Success />}/>
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+
+        {/* <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/success">
+          <Success />
+        </Route>
+        <Route path="/login">{user ? <Navigate to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Navigate to="/" /> : <Register />}
+        </Route> */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
